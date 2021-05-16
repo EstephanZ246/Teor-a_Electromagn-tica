d6ff3983b039dce107f6a78f1ed89ac4c77dacd0
Estephan2001 <franciscoestephan@gmail.com>
2021-05-16 10:18:55 -0600

    Update Código_Ejercicio_1_inciso_2.m

--- Código_Ejercicio_1_inciso_2.m
+++ Código_Ejercicio_1_inciso_2.m
@@ -2,3 +2,17 @@
 % TEORÍA ELECTROMAGNÉTICA
 % INTEGRANTES: 
 % - 
+
+
+Az = linspace(0,2*pi,50); %phi
+El = linspace(-pi,pi,50); %theta
+r = ones(1,50);
+
+%Construcción de malla
+[El,Az] = meshgrid(El,Az);
+r = meshgrid(r);
+
+%La conversión se realiza después de construir la malla
+[X,Y,Z] = sph2cart(Az,El,r);
+
+mesh(X,Y,Z), axis equal
