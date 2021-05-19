a0aeaf5865f2ca3a83e51fd6423eedc8abea657b
Estephan2001 <franciscoestephan@gmail.com>
2021-05-18 23:15:39 -0600

    Update Código_Ejercicio_1_inciso_2.m

--- Código_Ejercicio_1_inciso_2.m
+++ Código_Ejercicio_1_inciso_2.m
@@ -1,18 +1,9 @@
 % UNIVERSIDAD DEL VALLE DE GUATEMALA
 % TEORÍA ELECTROMAGNÉTICA
 % INTEGRANTES: 
-% - 
+% - Axel Mazariegos 
+% - Eliseo Lux
+% - Estephan Portales
 
 
-Az = linspace(0,2*pi,50); %phi
-El = linspace(-pi,pi,50); %theta
-r = ones(1,50);
 
-%Construcción de malla
-[El,Az] = meshgrid(El,Az);
-r = meshgrid(r);
-
-%La conversión se realiza después de construir la malla
-[X,Y,Z] = sph2cart(Az,El,r);
-
-mesh(X,Y,Z), axis equal
